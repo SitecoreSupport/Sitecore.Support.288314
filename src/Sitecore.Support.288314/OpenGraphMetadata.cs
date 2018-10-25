d670aba68b92dbfa7f1cab010aca503dfd24a3f9
DK\IRO <iro@sitecore.net>
2018-10-25 17:34:27 +0300

    Initial

---
+++ src/Sitecore.Support.288314/OpenGraphMetadata.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Repositories.OpenGraphMetadata
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.DependencyInjection;
+  using Sitecore.XA.Feature.SiteMetadata;
+  using Sitecore.XA.Feature.SiteMetadata.Models;
+  using Sitecore.XA.Feature.SiteMetadata.Repositories;
+  using Sitecore.XA.Feature.SiteMetadata.Services;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class OpenGraphMetadataRepository : Sitecore.XA.Feature.SiteMetadata.Repositories.OpenGraphMetadata.OpenGraphMetadataRepository
+  {
+    public OpenGraphMetadataRepository()
+    {
+      Mapping = new Dictionary<string, ID>
+        {
+            {
+                "og:description",
+                Templates._OpenGraphMetadata.Fields.OpenGraphDescription
+            },
+            {
+                "og:type",
+                Templates._OpenGraphMetadata.Fields.OpenGraphType
+            },
+            {
+                "og:site_name",
+                Templates._OpenGraphMetadata.Fields.OpenGraphSiteName
+            },
+            {
+                "og:admins",
+                Templates._OpenGraphMetadata.Fields.OpenGraphAdmins
+            },
+            {
+                "og:image",
+                Templates._OpenGraphMetadata.Fields.OpenGraphImageUrl
+            }
+        };
+    }
+  }
+}
---
+++ src/Sitecore.Support.288314/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.288314")]
+[assembly: AssemblyProduct("Sitecore.Support.288314")]
+[assembly: ComVisible(false)]
